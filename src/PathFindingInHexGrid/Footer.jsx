7601d117e8f68e1a84b6910267189cddb6533b64
Akhilesh Thurumalla <akhilesh,thurumalla@gmail.com>
2023-01-23 11:59:44 -0600

    update text

--- src/PathFindingInHexGrid/Footer.jsx
+++ src/PathFindingInHexGrid/Footer.jsx
@@ -8,15 +8,15 @@ function Footer({ selectedAlgorithm }) {
   // const [, updateText] = useState("Select an algorithm.");
   if (selectedAlgorithm == "BFS") {
     text =
-      "BFS algorithm works by traversing the graph level by level, visiting all the vertices in the current level before moving on to the next level, using a queue data structure to keep track of the vertices to be visited.";
+      "BFS algorithm works by traversing the graph level by level, visiting all the nodes in the current level before moving on to the next level, using a queue data structure to keep track of the nodes to be visited.";
   }
   if (selectedAlgorithm == "Dijstra's") {
     text =
-      "Dijkstras algorithm works by initializing a priority queue with the starting vertex, and repeatedly visiting the vertex with the smallest tentative distance (and marking it as visited) and updating the tentative distances of its neighbors.";
+      "Dijkstras algorithm works by initializing a priority queue with the starting node, and repeatedly visiting the node with the smallest tentative distance (and marking it as visited) and updating the tentative distances of its neighbors.";
   }
   if (selectedAlgorithm == "A*") {
     text =
-      "A* algorithm works by combining the strengths of Dijkstra's algorithm and a heuristic function, where it uses the heuristic function to guide the search towards the goal while keeping track of the actual cost from the starting point, using a priority queue to prioritize the vertices to be visited.";
+      "A* algorithm works by combining the strengths of Dijkstra's algorithm and a heuristic function, where it uses the heuristic function to guide the search towards the goal while keeping track of the actual cost from the starting node, using a priority queue to prioritize the nodes to be visited.";
   }
 
   return (
